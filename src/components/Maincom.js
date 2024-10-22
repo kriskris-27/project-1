f2cf0e659cbf3a4fa34633afd4ebe82cfaa488de
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-23 02:31:05 +0530

    basic structure initialized

---
+++ src/components/Maincom.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Maincom () {
+    return (
+        <>
+        <button>Web Design & No-Code Agency</button>
+        <h1>We Are Your Web Design & Development Partner</h1>
+        <div className="start-col">
+            <h5>
+            We craft high-performing,beautiful websites that are conversion-focused,brand-accurate,&people-firendly
+            </h5>
+            <button className="start-btn">Start A Project</button>
+            <button>more</button>
+        </div>
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3>LIMINALFLOW</h3>
+            <div>
+                <ul>
+                    <li>Services</li>
+                    <li>Projects</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
